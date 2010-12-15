28563a9ec7c6781daf45bde3c9819e09587f5aa2
ppcasm <ppcasm@hotmail.com>
2010-12-14 20:12:53 -0800

    Fixed pre-setup to help set up a better environment. First instance of being able to load code linked against libdragon.

--- gsuploader.c
+++ gsuploader.c
@@ -6,11 +6,10 @@ Modified version of HCS GSUpload - ppcasm
 #include <stdio.h>
 #include <conio.h>
 #define LPT1 0x378
+#define UPLOAD_ADDR 0x80300000 //Where to upload your code, and also where execution starts after embedded code runs.
 #define Out32(PortAddress, data) _outp(PortAddress, data)
 #define Inp32(PortAddress) _inp(PortAddress)
 #define TMOUT 0x4000 //This can probably be lower, it's set this high just for testing.
-#define UPLOAD_ADDR 0x80300000 //Where to upload your code, and also where execution starts after embedded code runs.
-#define EMBED_ADDR UPLOAD_ADDR-80 //Where to upload the embedded setup code, this runs first.
 #define INSN_PATCH_ADDR 0xA07C5C00 //GS Code Handler(uncached)
 #define makejump(address) ((unsigned long )(address)<<2>>4|0x08000000)
  
@@ -29,16 +28,10 @@ long GetFileSizez(FILE *input);
  
 int main(int argc, char ** argv)
 {
- 
+    /*Pull parport line low.*/
     Out32(LPT1, 0);
-       
-    unsigned long instruction=makejump(EMBED_ADDR);
-    unsigned char previnsn[4];
-    unsigned char insn[4];
-    insn[0]=instruction>>24&0xff;
-    insn[1]=instruction>>16&0xff;
-    insn[2]=instruction>>8&0xff;
-    insn[3]=instruction>>0&0xff;
+    
+
      
     printf("N64 HomeBrew Loader - ppcasm\n(Based on HCS GSUpload - http://here.is/halleyscomet)\n\n");
    
@@ -56,13 +49,39 @@ int main(int argc, char ** argv)
     }
    
     printf("File Size: %d bytes...\n", GetFileSizez(infile));  
-   
+    
+    /*Embedded pre-setup code*/
     unsigned char codebuf[]=
-    {0x3c, 0x1a, 0x00, 0x00,  //lui $k0, 0x0000 (will be modified later)
+    {
+     /*START: Stop gameshark traps.*/
+     0x40, 0x80, 0x90, 0x00,  //mtc0 $zero, watchlo
+     0x00, 0x00, 0x00, 0x00,  //nop
+     0x40, 0x80, 0x98, 0x00,  //mtc0 $zero, watchhi
+     0x00, 0x00, 0x00, 0x00,  //nop
+     /*END: Stop gameshark traps.*/
+     
+     /*START: Set Stack pointer to 0x803ffff0 for shop time.*/
+     0x3c, 0x1d, 0x80, 0x3f,   //lui $sp, 0x803f 
+     0x37, 0xbd, 0xff, 0xf0,   //ori $sp, $sp, 0xfff0
+        
+     /*START: Disable interrupts.*/
+     0x01, 0x00, 0xe8, 0x2d,   //move $sp, $t0
+     0x31, 0x09, 0xff, 0x00,   //andi $t1, $t0, 0xff00
+     0x01, 0x09, 0x40, 0x26,   //xor $t0, $t0, $t1
+     0x31, 0x08, 0xff, 0xfe,   //andi $t0, $t0, 0xfffe
+     0x40, 0x88, 0x60, 0x00,   //mtc0 $t0, $status
+     /*END: Disable interrupts*/
+     
+     /*START: Modify EPC.*/        
+     0x3c, 0x1a, 0x00, 0x00,  //lui $k0, 0x0000 (will be modified later)
      0x37, 0x5a, 0x00, 0x00,  //ori $k0, $k0, 0x0000 (will be modified later)
      0x00, 0x00, 0x00, 0x00,  //nop
      0x40, 0x9a, 0x70, 0x00,  //mtc0 $k0, $14
      0x00, 0x00, 0x00, 0x00,  //nop
+     /*END: Modify EPC.*/
+     
+     
+     /*START: Patch back modified code handler.*/
      0x3c, 0x1b, 0x3c, 0x1a,  //lui $k1, 0x3c1a
      0x37, 0x7b, 0x80, 0x00,  //ori $k1, $k1, 0x8000
      0x00, 0x00, 0x00, 0x00,  //nop
@@ -71,23 +90,32 @@ int main(int argc, char ** argv)
      0x00, 0x00, 0x00, 0x00,  //nop
      0xaf, 0x5b, 0x00, 0x00,  //sw $k1, 0($k0)
      0x00, 0x00, 0x00, 0x00,  //nop
-     0x3c, 0x1a, 0x80, 0x00,  //lui $k0, 0x8000
-     0x37, 0x5a, 0x01, 0x20,  //ori $k0, $k0, 0x120
-     0x03, 0x40, 0x00, 0x08,  //jr $k0
-     0x00, 0x00, 0x00, 0x00}; //nop
+     /*END: Patch back modified code handler.*/
+     
+     /*START: Return from interrupt.*/
+     0x42, 0x00, 0x00, 0x18,  //eret
+     0x00, 0x00, 0x00, 0x00,  //nop
+     /*END: Return from interrupt.*/
+     };
+     
+    /*Patch embedded code to load EPC register with usercode addr to go to after setup.*/
+      codebuf[46]=UPLOAD_ADDR>>24&0xff;
+      codebuf[47]=UPLOAD_ADDR>>16&0xff;
+      codebuf[50]=UPLOAD_ADDR>>8&0xff;
+      codebuf[51]=UPLOAD_ADDR>>0&0xff;
  
-    codebuf[2]=UPLOAD_ADDR>>24&0xff;
-    codebuf[3]=UPLOAD_ADDR>>16&0xff;
-    codebuf[6]=UPLOAD_ADDR>>8&0xff;
-    codebuf[7]=UPLOAD_ADDR>>0&0xff;
- 
+    /*Upload binary to specified address.*/
     if(UploadFile(infile,UPLOAD_ADDR))
     {
        printf("Uploading file failed...\n");
        Out32(LPT1, 0);
        return 1;
     }
- 
+    
+    /*Set up embedded code to right before usercode.*/
+    unsigned long EMBED_ADDR = UPLOAD_ADDR-sizeof(codebuf);
+    
+    /*Upload embedded code right before usercode.*/
     if(Upload(codebuf, sizeof(codebuf), EMBED_ADDR))
     {
        printf("Failed to upload embedded code...\n");
@@ -96,7 +124,18 @@ int main(int argc, char ** argv)
     }
    
     printf("Uploaded embedded code to: 0x%08x.\n", EMBED_ADDR);
-                       
+    
+    /*Make synthetic jump instruction based on address.*/
+    unsigned long instruction=makejump(EMBED_ADDR);
+    
+    /*Unload jump instruction into byte buffer for easy transfer.*/
+    unsigned char insn[4];
+    insn[0]=instruction>>24&0xff;
+    insn[1]=instruction>>16&0xff;
+    insn[2]=instruction>>8&0xff;
+    insn[3]=instruction>>0&0xff;
+    
+    /*Inject synthetic jump instruction into code handler to insure it runs.*/                   
     if(Upload(insn,4,INSN_PATCH_ADDR))
     {  
        printf("Instruction patch failed...\n");
