e6cac5d36f941af858bfa2ec4326fca419b53343
ppcasm <Administrator@.(none)>
2011-03-26 13:54:54 -0400

    yo

--- gsuploader.c
+++ gsuploader.c
@@ -1,9 +1,11 @@
-/*
-Modified version of HCS GSUpload - ppcasm
-(Compiled with Dev-Cpp 4.9.9.2)
-*/
-
 #include <stdio.h>
+#include "mips.h"
+#define LPT1 0xc400
+#define UPLOAD_ADDR 0xA0300000 //Where to upload your code, and also where execution starts after embedded code runs.
+#define GLOBAL_OFFSET_TABLE 0xA0500400 //Where to store exported function GOT.
+#define TMOUT 0x4000 //This can probably be lower, it's set this high just for testing.
+unsigned char GAMESHARK_COMMS_INIT_GOT[4];
+unsigned char GAMESHARK_CHECK_GSBUTTON_GOT[4];
 
 #ifdef LINUX
 #include <sys/io.h>
@@ -13,29 +15,152 @@ Modified version of HCS GSUpload - ppcasm
 #include <conio.h>
 #endif
 
-#define LPT1 0x378
-#define UPLOAD_ADDR 0x80300000 //Where to upload your code, and also where execution starts after embedded code runs.
-#define Out32(PortAddress, data) _outp(PortAddress, data)
-#define Inp32(PortAddress) _inp(PortAddress)
-#define TMOUT 0x4000 //This can probably be lower, it's set this high just for testing.
-#define INSN_PATCH_ADDR 0xA07C5C00 //GS Code Handler(uncached)
-#define makejump(address) ((unsigned long )(address)<<2>>4|0x08000000)
- 
-int InitGSComms(void);
-int CloseGSComms(void);
-int Upload(const unsigned char * buffer, unsigned long size, unsigned long address);
-int UploadFile(FILE * infile, unsigned long address);
-int Read(unsigned char * buffer, unsigned long size, unsigned long address);
-int GSFcn1(int);
-int SendNibble(int);
-int ReadWriteNibble(int);
-int ReadWriteByte(int);
-unsigned long ReadWrite32(unsigned long);
-int CheckGSPresence(void);
-long GetFileSizez(FILE *input);
- 
+
+/* Embedded pre-setup code
+   This is a lazy way of doing things, but it works. This code gets executed and sets
+   up the environment prior to loading your usercode.
+*/
+    unsigned long codebuf_pre[]=
+    {
+     
+    /* Stop GameShark traps */
+         MTC0(MIPS_R0, 18),
+         NOP,
+         MTC0(MIPS_R0, 19),
+         NOP,
+    
+    /* Disable Interrupts */
+        MFC0(MIPS_T0, 12),
+      	0x2409fffe, 
+        0x01094024, 
+      	MTC0(MIPS_T0, 12),
+         
+    /* Modify EPC */
+         LUI(MIPS_K0, UPLOAD_ADDR>>16), 
+         ORI(MIPS_K0, MIPS_K0, UPLOAD_ADDR), 
+         NOP,
+         MTC0(MIPS_K0, 14),
+         NOP,
+    
+    /* Patch back modified code handler */
+         LUI(MIPS_K1, 0x3c1a),
+         ORI(MIPS_K1, MIPS_K1, 0x8000),
+         NOP,
+         LUI(MIPS_K0, 0xa07c),
+         ORI(MIPS_K0, MIPS_K0, 0x5c00),
+         NOP,
+         SW(MIPS_K1, 0, MIPS_K0),
+         NOP,
+         SYNC,
+         NOP,
+         
+    /* Halt RSP */
+         LUI(MIPS_T1, 2),
+         LUI(MIPS_T0, 0xa404),
+         ORI(MIPS_T0, MIPS_T0, 0x0010),
+         NOP,
+         SW(MIPS_T1, 0, MIPS_T0),
+         NOP,
+         
+    /* Halt RDP */
+         LUI(MIPS_T1, 1|4|0x10|0x40|0x80|0x100|0x200),
+         LUI(MIPS_T0, 0xa410),
+         ORI(MIPS_T0, MIPS_T0, 0x000c),
+         NOP,
+         SW(MIPS_T1, 0, MIPS_T0),
+         NOP,
+         
+    /* Return from interrupt - execute code */
+         ERET,
+         NOP,
+         
+    /* Callback function: Start GScomms */
+        GAMESHARK_COMMS_INIT, //GScomms Marker
+        NOP,
+        MFC0(MIPS_T0, 12),
+      	0x2409fffe, 
+        0x01094024, 
+      	MTC0(MIPS_T0, 12),
+       	LUI(MIPS_V0,0xa000),
+        ORI(MIPS_A2,MIPS_V0,0x180),
+        LW(MIPS_A0,0,MIPS_A2),
+     	ORI(MIPS_V1,MIPS_V0,0x120),
+        SW(MIPS_A0,0, MIPS_V1),
+    	ORI(MIPS_A1,MIPS_V0,0x184),
+     	LW(MIPS_A0,0,MIPS_A1),
+     	ORI(MIPS_V1,MIPS_V0,0x124),
+     	SW(MIPS_A0,0,MIPS_V1),
+        ORI(MIPS_A0,MIPS_V0,0x188),
+    	LW(MIPS_A3,0,MIPS_A0),
+    	ORI(MIPS_V1,MIPS_V0,0x128),
+    	SW(MIPS_A3,0,MIPS_V1),
+      	ORI(MIPS_V1,MIPS_V0,0x18c),
+     	LW(MIPS_A3,0,MIPS_V1),
+     	ORI(MIPS_V0,MIPS_V0,0x12c),
+    	SW(MIPS_A3,0,MIPS_V0),
+    	LUI(MIPS_V0,0x3c1a),
+     	ORI(MIPS_V0,MIPS_V0,0xa079),
+     	SW(MIPS_V0,0,MIPS_A2),
+     	LUI(MIPS_V0,0x275a),
+     	ORI(MIPS_V0,MIPS_V0,0x4aec),
+    	SW(	MIPS_V0,0,MIPS_A1),
+     	LUI(MIPS_V0,0x340),
+    	ORI(MIPS_V0,MIPS_V0,0x8),
+    	SW(MIPS_V0,0,MIPS_A0),
+    	SW(MIPS_R0,0,MIPS_V1),
+    	LUI(MIPS_T1,0xa000),
+    	ORI(MIPS_T1,MIPS_T1,0x120),
+        CACHE(MIPS_T1, 0x10, 0),
+    	NOP,
+     	LUI(MIPS_T1,0xa000),
+    	ORI(MIPS_T1,MIPS_T1,0x124),
+     	CACHE(MIPS_T1, 0x10, 0),
+     	NOP,
+      	LUI(MIPS_T1,0xa000),
+      	ORI(MIPS_T1,MIPS_T1,0x128),
+      	CACHE(MIPS_T1, 0x10, 0),
+      	NOP,
+     	LUI(MIPS_T1,0xa000),
+      	ORI(MIPS_T1,MIPS_T1,0x12c),
+      	CACHE(MIPS_T1, 0x10, 0),
+      	NOP,
+      	LUI(MIPS_T1,0xa000),
+      	ORI(MIPS_T1,MIPS_T1,0x180),
+      	CACHE(MIPS_T1, 0x10, 0),
+      	NOP,
+      	LUI(MIPS_T1,0xa000),
+     	ORI(MIPS_T1,MIPS_T1,0x184),
+      	CACHE(MIPS_T1, 0x10, 0),
+      	NOP,
+      	LUI(MIPS_T1,0xa000),
+      	ORI(MIPS_T1,MIPS_T1,0x188),
+      	CACHE(MIPS_T1, 0x10, 0),
+      	NOP,
+      	LUI(MIPS_T1,0xa000),
+     	ORI(MIPS_T1,MIPS_T1,0x18c),
+    	CACHE(MIPS_T1, 0x10, 0),
+    	MFC0(MIPS_T0, 12),
+    	ORI(MIPS_T0, MIPS_T0, 1),
+    	MTC0(MIPS_T0, 12),
+        JR(MIPS_RA),    
+        NOP,
+        GAMESHARK_CHECK_GSBUTTON,
+        LUI(MIPS_V0, 0xbe40),
+        LBU(MIPS_V0, 0, MIPS_V0),
+        SRA(MIPS_V0, MIPS_V0, 0x2),
+        XORI(MIPS_V0, MIPS_V0, 0x1),
+        ANDI(MIPS_V0, MIPS_V0, 0x1),
+        JR(MIPS_RA),
+        NOP,
+   };
+   
 int main(int argc, char ** argv)
 {
+
+  //export_funcs(); //Build GOT export list for built in functions.
+  
+  int i = 0;
+  
 #ifdef LINUX
     int ret = ioperm(LPT1, 10, 1);
     if (ret < 0)
@@ -44,8 +169,6 @@ int main(int argc, char ** argv)
 
     /*Pull parport line low.*/
     Out32(LPT1, 0);
-    
-
      
     printf("N64 HomeBrew Loader - ppcasm\n(Based on HCS GSUpload - http://here.is/halleyscomet)\n\n");
    
@@ -63,61 +186,15 @@ int main(int argc, char ** argv)
     }
    
     printf("File Size: %d bytes...\n", GetFileSizez(infile));  
-    
-    /*Embedded pre-setup code*/
-    unsigned char codebuf[]=
+
+    unsigned char codebuf[sizeof(codebuf_pre)];   
+    for(i=0;i<=sizeof(codebuf_pre)/4;i++)
     {
-     /*START: Stop gameshark traps.*/
-     0x40, 0x80, 0x90, 0x00,  //mtc0 $zero, watchlo
-     0x00, 0x00, 0x00, 0x00,  //nop
-     0x40, 0x80, 0x98, 0x00,  //mtc0 $zero, watchhi
-     0x00, 0x00, 0x00, 0x00,  //nop
-     /*END: Stop gameshark traps.*/
-     
-     /*START: Set Stack pointer to 0x803ffff0 for shop time.*/
-     0x3c, 0x1d, 0x80, 0x3f,   //lui $sp, 0x803f 
-     0x37, 0xbd, 0xff, 0xf0,   //ori $sp, $sp, 0xfff0
-        
-     /*START: Disable interrupts.*/
-     0x01, 0x00, 0xe8, 0x2d,   //move $sp, $t0
-     0x31, 0x09, 0xff, 0x00,   //andi $t1, $t0, 0xff00
-     0x01, 0x09, 0x40, 0x26,   //xor $t0, $t0, $t1
-     0x31, 0x08, 0xff, 0xfe,   //andi $t0, $t0, 0xfffe
-     0x40, 0x88, 0x60, 0x00,   //mtc0 $t0, $status
-     /*END: Disable interrupts*/
-     
-     /*START: Modify EPC.*/        
-     0x3c, 0x1a, 0x00, 0x00,  //lui $k0, 0x0000 (will be modified later)
-     0x37, 0x5a, 0x00, 0x00,  //ori $k0, $k0, 0x0000 (will be modified later)
-     0x00, 0x00, 0x00, 0x00,  //nop
-     0x40, 0x9a, 0x70, 0x00,  //mtc0 $k0, $14
-     0x00, 0x00, 0x00, 0x00,  //nop
-     /*END: Modify EPC.*/
-     
-     
-     /*START: Patch back modified code handler.*/
-     0x3c, 0x1b, 0x3c, 0x1a,  //lui $k1, 0x3c1a
-     0x37, 0x7b, 0x80, 0x00,  //ori $k1, $k1, 0x8000
-     0x00, 0x00, 0x00, 0x00,  //nop
-     0x3c, 0x1a, 0xa0, 0x7c,  //lui $k0, 0xa07c
-     0x37, 0x5a, 0x5c, 0x00,  //ori $k0, $k0, 0x5c00
-     0x00, 0x00, 0x00, 0x00,  //nop
-     0xaf, 0x5b, 0x00, 0x00,  //sw $k1, 0($k0)
-     0x00, 0x00, 0x00, 0x00,  //nop
-     /*END: Patch back modified code handler.*/
-     
-     /*START: Return from interrupt.*/
-     0x42, 0x00, 0x00, 0x18,  //eret
-     0x00, 0x00, 0x00, 0x00,  //nop
-     /*END: Return from interrupt.*/
-     };
-     
-    /*Patch embedded code to load EPC register with usercode addr to go to after setup.*/
-      codebuf[46]=UPLOAD_ADDR>>24&0xff;
-      codebuf[47]=UPLOAD_ADDR>>16&0xff;
-      codebuf[50]=UPLOAD_ADDR>>8&0xff;
-      codebuf[51]=UPLOAD_ADDR>>0&0xff;
- 
+         codebuf_pre[i] = codebuf_pre[i]<<24&0xff000000|codebuf_pre[i]<<8&0x00ff0000|codebuf_pre[i]>>8&0x0000ff00|codebuf_pre[i]>>24&0xff; 
+    }
+    
+    memcpy(&codebuf, &codebuf_pre, sizeof(codebuf_pre)); 
+
     /*Upload binary to specified address.*/
     if(UploadFile(infile,UPLOAD_ADDR))
     {
@@ -128,7 +205,7 @@ int main(int argc, char ** argv)
     
     /*Set up embedded code to right before usercode.*/
     unsigned long EMBED_ADDR = UPLOAD_ADDR-sizeof(codebuf);
-    
+
     /*Upload embedded code right before usercode.*/
     if(Upload(codebuf, sizeof(codebuf), EMBED_ADDR))
     {
@@ -156,14 +233,64 @@ int main(int argc, char ** argv)
        Out32(LPT1, 0);
        return 1;    
     }
-   
+
     printf("Done.\n");
    
     Out32(LPT1, 0);
     close(infile);
-        return 0;
+  return 0;
 }
- 
+
+void export_funcs(void)
+{
+        printf("\nDbg: Function Callbacks:\n");
+
+        int i = 0;
+        for(i=0;i<=sizeof(codebuf_pre)/4;i++)
+        {
+             if(codebuf_pre[i]==GAMESHARK_COMMS_INIT)
+             {
+                  int address = UPLOAD_ADDR-sizeof(codebuf_pre)+i*4+4;
+                  unsigned long make_jump_got = J(address);
+                  GAMESHARK_COMMS_INIT_GOT[0]=make_jump_got>>24&0xff;
+                  GAMESHARK_COMMS_INIT_GOT[1]=make_jump_got>>16&0xff;
+                  GAMESHARK_COMMS_INIT_GOT[2]=make_jump_got>>8&0xff;
+                  GAMESHARK_COMMS_INIT_GOT[3]=make_jump_got>>0&0xff;
+            
+                  if(Upload(GAMESHARK_COMMS_INIT_GOT,4,GLOBAL_OFFSET_TABLE+GOT_OFFSET))
+                  {  
+                       printf("GOT patch failed...\n");
+                       Out32(LPT1, 0);
+                       return;    
+                  }
+                  
+                printf("0x%08x: void gscomms_init(void) - GOT: %x\n", address, GLOBAL_OFFSET_TABLE+GOT_OFFSET);
+                GOT_OFFSET+=4;
+             }
+             
+             if(codebuf_pre[i]==GAMESHARK_CHECK_GSBUTTON)
+             {
+                 int address = UPLOAD_ADDR-sizeof(codebuf_pre)+i*4+4;
+                 unsigned long make_jump_got = J(address);
+                 GAMESHARK_CHECK_GSBUTTON_GOT[0]=make_jump_got>>24&0xff;
+                 GAMESHARK_CHECK_GSBUTTON_GOT[1]=make_jump_got>>16&0xff;
+                 GAMESHARK_CHECK_GSBUTTON_GOT[2]=make_jump_got>>8&0xff;
+                 GAMESHARK_CHECK_GSBUTTON_GOT[3]=make_jump_got>>0&0xff;
+    
+                 if(Upload(GAMESHARK_CHECK_GSBUTTON_GOT,4,GLOBAL_OFFSET_TABLE+GOT_OFFSET))
+                 {  
+                     printf("GOT patch failed...\n");
+                     Out32(LPT1, 0);
+                     return;    
+                 }
+               printf("0x%08x: int check_gsbutton(void) - GOT: %x\n", address, GLOBAL_OFFSET_TABLE+GOT_OFFSET);
+               GOT_OFFSET+=4;
+             }
+        }
+     printf("\n");
+   return;
+}
+     
 long GetFileSizez(FILE *input)
 {
         long current, end;
@@ -241,7 +368,7 @@ int UploadFile(FILE * infile, unsigned long address) {
  
         if (InitGSComms()) return 1;
        
-    printf("Uploading File to %x | 00%%", UPLOAD_ADDR);
+    printf("Uploading File to %x | 00%%", address);
  
     ReadWriteByte(2);
         ReadWrite32(address);
